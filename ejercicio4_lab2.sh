f3155e867152e3c7192c032589c941f9bfe35072
Luis Rojas <lrojasmorua@gmail.com>
2024-09-15 15:59:50 -0600

    Script Ejercicio 4 Laboratorio 2

--- ejercicio4_lab2.sh
+++ ejercicio4_lab2.sh
@@ -3,7 +3,7 @@
 # Directorio a monitorear
 directorio="/home/luis/Descargas"
 
-datos="/home/luis/monitor_log.txt"
+datos="/home/luis/Laboratorios_Plataformas/monitoreo_log.txt"
 
 # Monitoreando con inotifywait y dando el formato
 inotifywait -m -r -e create -e modify -e delete "$directorio" --format '%T %w %f %e' --timefmt '%Y-%m-%d %H:%M:%S' | while read Fecha Hora directorio archivo  Evento
